7d5fed4dc2b3165207549d7ae55e5d8d140fe20e
Ingvar Sigurðsson <ingvar@programm.is>
2024-01-09 18:43:08 +0000

    Add markdown preview and treesitter markdown parser

--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -182,7 +182,12 @@ require('lazy').setup({
     'stevearc/oil.nvim',
     dependencies = { "nvim-tree/nvim-web-devicons" },
   },
-
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+  },
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
