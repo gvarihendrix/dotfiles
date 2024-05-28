b6685a31ac36bcdccb967ff640232871222f7f86
Ingvar Sigurðsson <gvarihendrix@gmail.com>
2024-05-28 22:51:34 +0000

    Fixing issues with config files
    Start using oil in nvim.
    Start using lazyextras instead of manually editing inserting plugins in init.lua
    Setting up better support for sesh.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,14 +8,15 @@ fnm --log-level quiet env --use-on-cd | source
 direnv hook fish | source
 set -g direnv_fish_mode eval_on_arrow # trigger direnv at prompt, and on every arrow-based directory change
 
-
-
 set -U fish_greeting
 set -U fish_key_binding fish_vi_key_binding
 
 set -Ux EDITOR nvim
 set -Ux FZF_DEFAULT_COMMAND "fd -H -E '.git'"
 set -Ux VISUAL nvim
+
+
+fish_add_path $HOME/.config/bin
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
--- .config/nvim/dev.ps1
+++ .config/nvim/dev.ps1
@@ -2,5 +2,5 @@ if (Test-Path -Path $env:LOCALAPPDATA\nvim) {
 	Remove-Item $env:LOCALAPPDATA\nvim -Recurse
 }
 
-Copy-Item $env:USERPROFILE\dotfiles\nvim -Destination $env:LOCALAPPDATA\nvim -Recurse
+Copy-Item $(pwd) -Destination $env:LOCALAPPDATA\nvim -Recurse
 
