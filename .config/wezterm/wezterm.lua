1636589aa41f293591a71125bc6704b36f276fa7
Ingvar Sigurðsson <gvarihendrix@gmail.com>
2024-05-29 00:24:51 +0000

    Making wezterm more tmux friendly
    Added keybindings for tmux and local wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,16 +1,196 @@
-local wezterm = require('wezterm')
-local _ = require('status_bar')
+local wezterm = require("wezterm")
+local act = wezterm.action
+local _ = require("status_bar")
+local k = require("utils/keys")
 
-local config = {}
+local config = {
+	default_prog = { "/opt/homebrew/bin/fish", "-l" },
+	color_scheme = "rose-pine",
+	font = wezterm.font("JetBrains Mono", { weight = "Bold" }),
+	font_size = 16.0,
+	inactive_pane_hsb = {
+		hue = 1.0,
+		saturation = 1.0,
+		brightness = 1.0,
+	},
+	launch_menu = {},
+	enable_tab_bar = false,
+	disable_default_key_bindings = true,
+	leader = { key = "a", mods = "CTRL" },
+	keys = {
+		-- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+		{ key = "a", mods = "LEADER|CTRL", action = wezterm.action({ SendString = "\x01" }) },
+		{ key = "-", mods = "LEADER", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
+		{
+			key = "\\",
+			mods = "LEADER",
+			action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }),
+		},
+		{ key = "z", mods = "LEADER", action = "TogglePaneZoomState" },
+		{ key = "c", mods = "LEADER", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
+		{ key = "h", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
+		{ key = "j", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
+		{ key = "k", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
+		{ key = "l", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
+		{ key = "H", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Left", 5 } }) },
+		{ key = "J", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Down", 5 } }) },
+		{ key = "K", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Up", 5 } }) },
+		{ key = "L", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Right", 5 } }) },
+		{ key = "1", mods = "LEADER", action = wezterm.action({ ActivateTab = 0 }) },
+		{ key = "2", mods = "LEADER", action = wezterm.action({ ActivateTab = 1 }) },
+		{ key = "3", mods = "LEADER", action = wezterm.action({ ActivateTab = 2 }) },
+		{ key = "4", mods = "LEADER", action = wezterm.action({ ActivateTab = 3 }) },
+		{ key = "5", mods = "LEADER", action = wezterm.action({ ActivateTab = 4 }) },
+		{ key = "6", mods = "LEADER", action = wezterm.action({ ActivateTab = 5 }) },
+		{ key = "7", mods = "LEADER", action = wezterm.action({ ActivateTab = 6 }) },
+		{ key = "8", mods = "LEADER", action = wezterm.action({ ActivateTab = 7 }) },
+		{ key = "9", mods = "LEADER", action = wezterm.action({ ActivateTab = 8 }) },
+		{ key = "&", mods = "LEADER|SHIFT", action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
+		{ key = "x", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
 
-if wezterm.config_builder then
-	config = wezterm.config_builder()
+		{ key = "n", mods = "SHIFT|CTRL", action = "ToggleFullScreen" },
+		{ key = "v", mods = "SHIFT|CTRL", action = wezterm.action.PasteFrom("Clipboard") },
+		{ key = "c", mods = "SHIFT|CTRL", action = wezterm.action.CopyTo("Clipboard") },
+		{ key = "+", mods = "SHIFT|CTRL", action = "IncreaseFontSize" },
+		{ key = "-", mods = "SHIFT|CTRL", action = "DecreaseFontSize" },
+		{ key = "0", mods = "SHIFT|CTRL", action = "ResetFontSize" },
+		k.cmd_key(".", k.multiple_actions(":ZenMode")),
+		k.cmd_key("[", act.SendKey({ mods = "CTRL", key = "o" })),
+		k.cmd_key("]", act.SendKey({ mods = "CTRL", key = "i" })),
+		k.cmd_key("f", k.multiple_actions(":Grep")),
+		-- k.cmd_key("H", act.SendKey({ mods = "CTRL", key = "h" })),
+		k.cmd_key("i", k.multiple_actions(":SmartGoTo")),
+		-- k.cmd_key("J", act.SendKey({ mods = "CTRL", key = "j" })),
+		-- k.cmd_key("K", act.SendKey({ mods = "CTRL", key = "k" })),
+		-- k.cmd_key("K", act.SendKey({ mods = "CTRL", key = "k" })),
+		-- k.cmd_key("L", act.SendKey({ mods = "CTRL", key = "l" })),
+		k.cmd_key("O", k.multiple_actions(":GoToSymbol")),
+		k.cmd_key("P", k.multiple_actions(":GoToCommand")),
+		k.cmd_key("p", k.multiple_actions(":GoToFile")),
+		k.cmd_key("q", k.multiple_actions(":qa!")),
+		k.cmd_to_tmux_prefix("1", "1"),
+		k.cmd_to_tmux_prefix("2", "2"),
+		k.cmd_to_tmux_prefix("3", "3"),
+		k.cmd_to_tmux_prefix("4", "4"),
+		k.cmd_to_tmux_prefix("5", "5"),
+		k.cmd_to_tmux_prefix("6", "6"),
+		k.cmd_to_tmux_prefix("7", "7"),
+		k.cmd_to_tmux_prefix("8", "8"),
+		k.cmd_to_tmux_prefix("9", "9"),
+		k.cmd_to_tmux_prefix("`", "n"),
+		k.cmd_to_tmux_prefix("b", "b"),
+		k.cmd_to_tmux_prefix("C", "C"),
+		k.cmd_to_tmux_prefix("d", "D"),
+		k.cmd_to_tmux_prefix("G", "G"),
+		k.cmd_to_tmux_prefix("g", "g"),
+		k.cmd_to_tmux_prefix("j", "J"),
+		k.cmd_to_tmux_prefix("K", "T"),
+		k.cmd_to_tmux_prefix("k", "K"),
+		k.cmd_to_tmux_prefix("l", "L"),
+		k.cmd_to_tmux_prefix("n", '"'),
+		k.cmd_to_tmux_prefix("N", "%"),
+		k.cmd_to_tmux_prefix("o", "u"),
+		k.cmd_to_tmux_prefix("T", "!"),
+		k.cmd_to_tmux_prefix("Y", "Y"),
+		k.cmd_to_tmux_prefix("t", "c"),
+		k.cmd_to_tmux_prefix("w", "x"),
+		k.cmd_to_tmux_prefix("z", "z"),
+		k.cmd_to_tmux_prefix("Z", "Z"),
+
+		k.cmd_key(
+			"R",
+			act.Multiple({
+				act.SendKey({ key = "\x1b" }), -- escape
+				k.multiple_actions(":source %"),
+			})
+		),
+
+		k.cmd_key(
+			"s",
+			act.Multiple({
+				act.SendKey({ key = "\x1b" }), -- escape
+				k.multiple_actions(":w"),
+			})
+		),
+
+		{
+			mods = "CMD|SHIFT",
+			key = "}",
+			action = act.Multiple({
+				act.SendKey({ mods = "CTRL", key = "b" }),
+				act.SendKey({ key = "n" }),
+			}),
+		},
+		{
+			mods = "CMD|SHIFT",
+			key = "{",
+			action = act.Multiple({
+				act.SendKey({ mods = "CTRL", key = "b" }),
+				act.SendKey({ key = "p" }),
+			}),
+		},
+
+		{
+			mods = "CTRL",
+			key = "Tab",
+			action = act.Multiple({
+				act.SendKey({ mods = "CTRL", key = "b" }),
+				act.SendKey({ key = "n" }),
+			}),
+		},
+
+		{
+			mods = "CTRL|SHIFT",
+			key = "Tab",
+			action = act.Multiple({
+				act.SendKey({ mods = "CTRL", key = "b" }),
+				act.SendKey({ key = "n" }),
+			}),
+		},
+
+		-- FIX: disable binding
+		-- {
+		-- 	mods = "CMD",
+		-- 	key = "`",
+		-- 	action = act.Multiple({
+		-- 		act.SendKey({ mods = "CTRL", key = "b" }),
+		-- 		act.SendKey({ key = "n" }),
+		-- 	}),
+		-- },
+
+		{
+			mods = "CMD",
+			key = "~",
+			action = act.Multiple({
+				act.SendKey({ mods = "CTRL", key = "b" }),
+				act.SendKey({ key = "p" }),
+			}),
+		},
+	},
+	set_environment_variables = {},
+}
+
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	-- config.front_end = "Software" -- OpenGL doesn't work quite well with RDP.
+	-- config.term = "" -- Set to empty so FZF works on windows
+	table.insert(config.launch_menu, { label = "PowerShell", args = { "powershell.exe", "-NoLogo" } })
+
+	-- Find installed visual studio version(s) and add their compilation
+	-- environment command prompts to the menu
+	for _, vsvers in ipairs(wezterm.glob("Microsoft Visual Studio/20*", "C:/Program Files (x86)")) do
+		local year = vsvers:gsub("Microsoft Visual Studio/", "")
+		table.insert(config.launch_menu, {
+			label = "x64 Native Tools VS " .. year,
+			args = {
+				"cmd.exe",
+				"/k",
+				"C:/Program Files (x86)/" .. vsvers .. "/BuildTools/VC/Auxiliary/Build/vcvars64.bat",
+			},
+		})
+	end
+else
+	table.insert(config.launch_menu, { label = "bash", args = { "bash", "-l" } })
+	table.insert(config.launch_menu, { label = "fish", args = { "fish", "-l" } })
 end
-
-config.default_prog = { '/opt/homebrew/bin/fish', '-l' }
-
-config.color_scheme = 'rose-pine'
-config.font = wezterm.font('JetBrains Mono', { weight = 'Bold' })
-config.font_size = 12.0
 
 return config
