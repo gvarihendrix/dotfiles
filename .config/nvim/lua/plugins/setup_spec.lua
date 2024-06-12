32552a474cab0b2c64a323ad4d5c06067f0ef9df
Ingvar Sigurðsson <gvarihendrix@gmail.com>
2024-06-12 22:12:02 +0000

    new bin files + some fixups
    * Remove mini-starter
    * Use default dashboard

--- .config/nvim/lua/plugins/setup_spec.lua
+++ .config/nvim/lua/plugins/setup_spec.lua
@@ -112,49 +112,17 @@ return {
     "neovim/nvim-lspconfig",
     ---@class PluginLspOpts
     opts = {
-      ---@type lspconfig.options
+      setup = {
+        rust_analyzer = function()
+          return true
+        end,
+      },
+      ---@type lsp.ConfigurationItem
       servers = {
-        -- pyright will be automatically installed with mason and loaded with lspconfig
         pyright = {},
       },
     },
   },
-
-  -- add tsserver and setup with typescript.nvim instead of lspconfig
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   dependencies = {
-  --     "jose-elias-alvarez/typescript.nvim",
-  --     init = function()
-  --       require("lazyvim.util").lsp.on_attach(function(_, buffer)
-  --         -- stylua: ignore
-  --         vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
-  --         vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
-  --       end)
-  --     end,
-  --   },
-  --   ---@class PluginLspOpts
-  --   opts = {
-  --     ---@type lspconfig.options
-  --     servers = {
-  --       -- tsserver will be automatically installed with mason and loaded with lspconfig
-  --       tsserver = {},
-  --     },
-  --     -- you can do any additional lsp server setup here
-  --     -- return true if you don't want this server to be setup with lspconfig
-  --     ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
-  --     setup = {
-  --       -- example to setup with typescript.nvim
-  --       tsserver = function(_, opts)
-  --         require("typescript").setup({ server = opts })
-  --         return true
-  --       end,
-  --       -- Specify * to use this function as a fallback for any server
-  --       -- ["*"] = function(server, opts) end,
-  --     },
-  --   },
-  -- },
-
   -- since `vim.tbl_deep_extend`, can only merge tables and not lists, the code above
   -- would overwrite `ensure_installed` with the new value.
   -- If you'd rather extend the default config, use the code below instead:
@@ -178,27 +146,10 @@ return {
         "typescript",
         "vim",
         "yaml",
+        "zig",
         "tsx",
         "typescript",
       })
-    end,
-  },
-
-  {
-    "kristijanhusak/vim-dadbod-ui",
-    dependencies = {
-      { "tpope/vim-dadbod", lazy = true },
-      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
-    },
-    cmd = {
-      "DBUI",
-      "DBUIToggle",
-      "DBUIAddConnection",
-      "DBUIFindBuffer",
-    },
-    init = function()
-      -- Your DBUI configuration
-      vim.g.db_ui_use_nerd_fonts = 1
     end,
   },
 
