f848ba6e8e38538f642a010a8790a4a8305659d0
Ingvar Sigurðsson <gvarihendrix@gmail.com>
2024-10-31 19:51:11 +0000

    fix(indent-blankline): Fix issue with indent-blankline

--- .config/nvim/lua/plugins/neorg.lua
+++ .config/nvim/lua/plugins/neorg.lua
@@ -1,40 +1,34 @@
 return {
   "nvim-neorg/neorg",
-  ft = "norg",
+  -- Only use if you want to layz load neorg
+  -- ft = "norg",
   version = "*",
-  config = function()
-    require("neorg").setup({
-      load = {
-        ["core.defaults"] = {},
-        ["core.concealer"] = {},
-        -- ["core.keybinds"] = {
-        --   default_keybinds = false
-        -- }
-        ["core.dirman"] = {
-          config = {
-            workspaces = {
-              notes = "~/notes",
-            },
-            default_workspace = "notes",
+  opts = {
+    load = {
+      ["core.defaults"] = {},
+      -- ["core.keybinds"] = {
+      --   default_keybinds = false
+      -- }
+      ["core.dirman"] = {
+        config = {
+          workspaces = {
+            notes = "~/notes",
           },
-        },
-        -- ["core.intergrations.treesitter"] = {},
-        -- ["core.autocommands"] = {},
-        -- ["core.esupports.indent"] = {},
-        -- ["core.completion"] = {},
-        -- ["core.intergrations.nvim-cmp"] = {},
-        ["core.ui"] = {},
-        ["core.queries.native"] = {},
-        ["core.presenter"] = {
-          config = {
-            zen_mode = "zen-mode",
-          },
+          default_workspace = "notes",
         },
       },
-    })
-
+      ["core.concealer"] = {},
+      ["core.ui"] = {},
+      ["core.queries.native"] = {},
+      ["core.presenter"] = {
+        config = {
+          zen_mode = "zen-mode",
+        },
+      },
+    },
+  },
+  init = function()
     vim.wo.foldlevel = 99
     vim.wo.conceallevel = 2
   end,
-  build = ":Neorg sync-parsers",
 }
