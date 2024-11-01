4553262f3cb332b742287f50cd5f7aa4094b21a0
Ingvar Sigurðsson <gvarihendrix@gmail.com>
2024-11-01 13:32:19 +0000

    Split up setup_spec.lua a little bit

---
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      setup = {
+        rust_analyzer = function()
+          return true
+        end,
+      },
+      ---@type lsp.ConfigurationItem
+      servers = {
+        pyright = {},
+      },
+    },
+  },
+}
