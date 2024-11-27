491eddf0beb5e5c3f42b502c25f5f01881b90764
Ingvar Sigurðsson <gvarihendrix@gmail.com>
2024-11-27 21:37:35 +0000

    fix(cleanup): Cleanup in wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -11,8 +11,9 @@ end
 
 config.default_prog = { "/opt/homebrew/bin/fish", "-l" }
 config.color_scheme = "Catppuccin Mocha"
-config.font = wezterm.font("JetBrains Mono", { weight = "Bold" })
-config.font_size = 14.0
+-- config.font = wezterm.font("JetBrains Mono", { weight = "Bold" })
+config.font = wezterm.font("Source Code Pro Medium")
+config.font_size = 13.0
 config.inactive_pane_hsb = {
 	hue = 1.0,
 	saturation = 1.0,
@@ -74,7 +75,7 @@ config.keys = {
 	{ key = "l", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
 	{ key = "H", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Left", 5 } }) },
 	{ key = "J", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Down", 5 } }) },
-	{ key = "K", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Up", 5 } }) },
+	{ key = "K", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Up", 3 } }) },
 	{ key = "L", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Right", 5 } }) },
 	{ key = "1", mods = "LEADER", action = wezterm.action({ ActivateTab = 0 }) },
 	{ key = "2", mods = "LEADER", action = wezterm.action({ ActivateTab = 1 }) },
@@ -109,151 +110,13 @@ config.keys = {
 
 	{ key = "w", mods = "LEADER", action = wezterm.action.ShowTabNavigator },
 
-	-- CTRL + (h,j,k,l) to move between panes
-	{
-		key = "h",
-		mods = "CTRL",
-		action = act({ EmitEvent = "move-left" }),
-	},
-	{
-		key = "j",
-		mods = "CTRL",
-		action = act({ EmitEvent = "move-down" }),
-	},
-	{
-		key = "k",
-		mods = "CTRL",
-		action = act({ EmitEvent = "move-up" }),
-	},
-	{
-		key = "l",
-		mods = "CTRL",
-		action = act({ EmitEvent = "move-right" }),
-	},
-	-- ALT + (h,j,k,l) to resize panes
-	{
-		key = "h",
-		mods = "ALT",
-		action = act({ EmitEvent = "resize-left" }),
-	},
-	{
-		key = "j",
-		mods = "ALT",
-		action = act({ EmitEvent = "resize-down" }),
-	},
-	{
-		key = "k",
-		mods = "ALT",
-		action = act({ EmitEvent = "resize-up" }),
-	},
-	{
-		key = "l",
-		mods = "ALT",
-		action = act({ EmitEvent = "resize-right" }),
-	},
-
 	k.cmd_key(".", k.multiple_actions(":ZenMode")),
-	-- k.cmd_key("[", act.SendKey({ mods = "CTRL", key = "o" })),
-	-- k.cmd_key("]", act.SendKey({ mods = "CTRL", key = "i" })),
 	k.cmd_key("f", k.multiple_actions(":Grep")),
-	-- k.cmd_key("H", act.SendKey({ mods = "CTRL", key = "h" })),
 	k.cmd_key("i", k.multiple_actions(":SmartGoTo")),
-	-- k.cmd_key("J", act.SendKey({ mods = "CTRL", key = "j" })),
-	-- k.cmd_key("K", act.SendKey({ mods = "CTRL", key = "k" })),
-	-- k.cmd_key("K", act.SendKey({ mods = "CTRL", key = "k" })),
-	-- k.cmd_key("L", act.SendKey({ mods = "CTRL", key = "l" })),
 	k.cmd_key("O", k.multiple_actions(":GoToSymbol")),
 	k.cmd_key("P", k.multiple_actions(":GoToCommand")),
 	k.cmd_key("p", k.multiple_actions(":GoToFile")),
 	k.cmd_key("q", k.multiple_actions(":qa!")),
-	-- k.cmd_to_tmux_prefix("1", "1"),
-	-- k.cmd_to_tmux_prefix("2", "2"),
-	-- k.cmd_to_tmux_prefix("3", "3"),
-	-- k.cmd_to_tmux_prefix("4", "4"),
-	-- k.cmd_to_tmux_prefix("5", "5"),
-	-- k.cmd_to_tmux_prefix("6", "6"),
-	-- k.cmd_to_tmux_prefix("7", "7"),
-	-- k.cmd_to_tmux_prefix("8", "8"),
-	-- k.cmd_to_tmux_prefix("9", "9"),
-	-- k.cmd_to_tmux_prefix("`", "n"),
-	-- k.cmd_to_tmux_prefix("b", "b"),
-	-- k.cmd_to_tmux_prefix("C", "C"),
-	-- k.cmd_to_tmux_prefix("d", "D"),
-	-- k.cmd_to_tmux_prefix("G", "G"),
-	-- k.cmd_to_tmux_prefix("g", "g"),
-	-- k.cmd_to_tmux_prefix("j", "J"),
-	-- k.cmd_to_tmux_prefix("K", "T"),
-	-- k.cmd_to_tmux_prefix("k", "K"),
-	-- k.cmd_to_tmux_prefix("l", "L"),
-	-- k.cmd_to_tmux_prefix("n", '"'),
-	-- k.cmd_to_tmux_prefix("N", "%"),
-	-- k.cmd_to_tmux_prefix("o", "u"),
-	-- k.cmd_to_tmux_prefix("T", "!"),
-	-- k.cmd_to_tmux_prefix("Y", "Y"),
-	-- k.cmd_to_tmux_prefix("t", "c"),
-	-- k.cmd_to_tmux_prefix("w", "x"),
-	-- k.cmd_to_tmux_prefix("z", "z"),
-	-- k.cmd_to_tmux_prefix("Z", "Z"),
-
-	-- k.cmd_key(
-	-- 	"R",
-	-- 	act.Multiple({
-	-- 		act.SendKey({ key = "\x1b" }), -- escape
-	-- 		k.multiple_actions(":source %"),
-	-- 	})
-	-- ),
-
-	-- k.cmd_key(
-	-- 	"s",
-	-- 	act.Multiple({
-	-- 		act.SendKey({ key = "\x1b" }), -- escape
-	-- 		k.multiple_actions(":w"),
-	-- 	})
-	-- ),
-
-	-- {
-	-- 	mods = "CMD|SHIFT",
-	-- 	key = "}",
-	-- 	action = act.Multiple({
-	-- 		act.SendKey({ mods = "CTRL", key = "b" }),
-	-- 		act.SendKey({ key = "n" }),
-	-- 	}),
-	-- },
-	-- {
-	-- 	mods = "CMD|SHIFT",
-	-- 	key = "{",
-	-- 	action = act.Multiple({
-	-- 		act.SendKey({ mods = "CTRL", key = "b" }),
-	-- 		act.SendKey({ key = "p" }),
-	-- 	}),
-	-- },
-
-	-- {
-	-- 	mods = "CTRL",
-	-- 	key = "Tab",
-	-- 	action = act.Multiple({
-	-- 		act.SendKey({ mods = "CTRL", key = "b" }),
-	-- 		act.SendKey({ key = "n" }),
-	-- 	}),
-	-- },
-
-	-- {
-	-- 	mods = "CTRL|SHIFT",
-	-- 	key = "Tab",
-	-- 	action = act.Multiple({
-	-- 		act.SendKey({ mods = "CTRL", key = "b" }),
-	-- 		act.SendKey({ key = "n" }),
-	-- 	}),
-	-- },
-
-	-- {
-	-- 	mods = "CMD",
-	-- 	key = "~",
-	-- 	action = act.Multiple({
-	-- 		act.SendKey({ mods = "CTRL", key = "b" }),
-	-- 		act.SendKey({ key = "p" }),
-	-- 	}),
-	-- },
 }
 -- 	set_environment_variables = {},
 
