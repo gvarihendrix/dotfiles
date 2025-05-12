dc2ffe1de78c92626900b462f220cb677e5da29a
Ingvar Sigurðsson <ingvar@apro.is>
2025-05-12 15:56:43 +0000

    fix: take in update of mason bug from lazyvim

--- .config/nvim/lua/plugins/mason-workaround.lua
+++
@@ -1,5 +0,0 @@
--- workaround while the 2.0 fixes are comming into lazyvim
-return {
-  { "mason-org/mason.nvim", version = "^1.0.0" },
-  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
-}
