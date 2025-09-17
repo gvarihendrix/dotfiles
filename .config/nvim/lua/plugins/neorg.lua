2b9b1e8a8fc8f14f43048dbcd275014953e57312
Ingvar Sigurðsson <ingvar@apro.is>
2025-09-17 15:45:29 +0000

    fix: updates and disble what broke

--- .config/nvim/lua/plugins/neorg.lua
+++ .config/nvim/lua/plugins/neorg.lua
@@ -1,34 +1,36 @@
-return {
-  "nvim-neorg/neorg",
-  -- Only use if you want to layz load neorg
-  -- ft = "norg",
-  version = "*",
-  opts = {
-    load = {
-      ["core.defaults"] = {},
-      -- ["core.keybinds"] = {
-      --   default_keybinds = false
-      -- }
-      ["core.dirman"] = {
-        config = {
-          workspaces = {
-            notes = "~/notes",
-          },
-          default_workspace = "notes",
-        },
-      },
-      ["core.concealer"] = {},
-      ["core.ui"] = {},
-      ["core.queries.native"] = {},
-      ["core.presenter"] = {
-        config = {
-          zen_mode = "zen-mode",
-        },
-      },
-    },
-  },
-  init = function()
-    vim.wo.foldlevel = 99
-    vim.wo.conceallevel = 2
-  end,
-}
+-- Neorg seems to be broken, disble for now
+return {}
+-- return {
+--   "nvim-neorg/neorg",
+--   -- Only use if you want to layz load neorg
+--   -- ft = "norg",
+--   version = "*",
+--   opts = {
+--     load = {
+--       ["core.defaults"] = {},
+--       -- ["core.keybinds"] = {
+--       --   default_keybinds = false
+--       -- }
+--       ["core.dirman"] = {
+--         config = {
+--           workspaces = {
+--             notes = "~/notes",
+--           },
+--           default_workspace = "notes",
+--         },
+--       },
+--       ["core.concealer"] = {},
+--       ["core.ui"] = {},
+--       ["core.queries.native"] = {},
+--       ["core.presenter"] = {
+--         config = {
+--           zen_mode = "zen-mode",
+--         },
+--       },
+--     },
+--   },
+--   init = function()
+--     vim.wo.foldlevel = 99
+--     vim.wo.conceallevel = 2
+--   end,
+-- }
